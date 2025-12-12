3fc6f471d3977f501db61eb26b084cddc62597b6
NotSilent <keiya94@gmail.com>
2025-12-12 23:46:34 +0100

    Basic connection and disconnection

--- crates/server/src/main.rs
+++ crates/server/src/main.rs
@@ -1,17 +1,95 @@
-use std::{io::Read, net::UdpSocket};
+use std::{
+    net::{SocketAddr, UdpSocket},
+    thread,
+    time::{Duration, Instant},
+};
+
+const TIMEOUT: f32 = 5.0;
+
+struct Connection {
+    timeout: f32,
+    src_addr: SocketAddr,
+}
+
+impl Connection {
+    pub fn new(src_addr: SocketAddr) -> Self {
+        Self {
+            timeout: TIMEOUT,
+            src_addr,
+        }
+    }
+}
+
+#[derive(Default)]
+struct Connections(Vec<Connection>);
+
+impl Connections {
+    fn drop_timeouts(&mut self, delta_time: f32) {
+        let mut index = 0;
+        while index < self.0.len() {
+            let connection = &mut self.0[index];
+            connection.timeout -= delta_time;
+
+            if connection.timeout < 0.0 {
+                println!("Client timeout: {}", connection.src_addr);
+                self.0.swap_remove(index);
+            }
+
+            index += 1;
+        }
+    }
+
+    fn update(&mut self, src_addr: &SocketAddr) {
+        let index = self.0.iter().position(|c| c.src_addr == *src_addr);
+        let index = if let Some(index) = index {
+            index
+        } else {
+            let new_connection = Connection::new(*src_addr);
+            self.0.push(new_connection);
+
+            self.0.len() - 1
+        };
+
+        let conection = &mut self.0[index];
+        conection.timeout = TIMEOUT;
+    }
+}
 
 fn main() {
+    let mut connections = Connections::default();
+
     let address = "127.0.0.1";
     let port = "7777";
     let socket = UdpSocket::bind(format!("{address}:{port}")).expect("Couldn't bind to address");
+    socket.set_nonblocking(true).unwrap();
+
+    println!("Server started");
 
     let mut buf = [0u8; 1024];
 
-    loop {
-        let (number_of_bytes, src_addr) = socket.recv_from(&mut buf).expect("Didn't receive data");
-        let data = &buf[..number_of_bytes];
+    let timestep = Duration::from_secs_f64(1.0 / 20.0);
 
-        handle_packet(data);
+    println!("====");
+    println!("Timestep: {}", timestep.as_secs_f64());
+    println!("====\n");
+
+    loop {
+        let start = Instant::now();
+
+        while let Ok((number_of_bytes, src_addr)) = socket.recv_from(&mut buf) {
+            let data = &buf[..number_of_bytes];
+
+            handle_packet(data);
+            connections.update(&src_addr);
+        } // TODO: Error handling
+
+        connections.drop_timeouts(timestep.as_secs_f32());
+
+        let duration = start.elapsed();
+
+        // println!("Server tick: {:.8}", duration.as_secs_f64());
+
+        thread::sleep(timestep - duration);
     }
 }
 
