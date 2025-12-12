44f40ebba6f116e157a19347b696f5bbbc7f534b
NotSilent <keiya94@gmail.com>
2025-12-12 14:04:46 +0100

    Basic udp connection

--- crates/client/src/main.rs
+++ crates/client/src/main.rs
@@ -1,3 +1,20 @@
+use std::{net::UdpSocket, thread, time::Duration};
+
 fn main() {
-    println!("Hello, world!");
+    let address = "127.0.0.1";
+    let port = "7777";
+    let socket = UdpSocket::bind(format!("{address}:0")).expect("Couldn't bind to address");
+    socket
+        .connect(format!("{address}:{port}"))
+        .expect("Couldn't connect to server");
+
+    let mut buf = [0u8; 1024];
+
+    loop {
+        socket
+            .send("Hello, server!".as_bytes())
+            .expect("Couldn't send packet");
+
+        thread::sleep(Duration::from_secs(1));
+    }
 }
--- crates/server/src/main.rs
+++ crates/server/src/main.rs
@@ -1,3 +1,23 @@
+use std::{io::Read, net::UdpSocket};
+
 fn main() {
-    println!("Hello, world!");
+    let address = "127.0.0.1";
+    let port = "7777";
+    let socket = UdpSocket::bind(format!("{address}:{port}")).expect("Couldn't bind to address");
+
+    let mut buf = [0u8; 1024];
+
+    loop {
+        let (number_of_bytes, src_addr) = socket.recv_from(&mut buf).expect("Didn't receive data");
+        let data = &buf[..number_of_bytes];
+
+        handle_packet(data);
+    }
+}
+
+fn handle_packet(data: &[u8]) {
+    let text = String::from_utf8_lossy(data);
+
+    println!("Hello, Client!");
+    println!("Received message: {text}");
 }
